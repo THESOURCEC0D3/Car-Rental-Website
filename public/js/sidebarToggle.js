547ffbd8939a3ee3ea6422be567a425bdb479f9b
THESOURCEC0D3 <jonathantamunoiduari@gmail.com>
2025-12-12 20:20:30 +0100

    Just finished up the sidebar and the overlay

--- public/js/sidebarToggle.js
+++ public/js/sidebarToggle.js
@@ -1,21 +1,43 @@
 const menuBtn = document.getElementById("menubtn");
 const mobileMenu = document.getElementById("mobileMenu");
 const closeBtn = document.getElementById("closeBtn");
+const overlay = document.getElementById("overlay");
 const menuLinks = document.querySelectorAll(".menu-link");
 
 menuBtn.addEventListener("click", () => {
   mobileMenu.classList.remove("translate-x-full");
   menuBtn.classList.add("hidden");
+   overlay.classList.remove("opacity-0");
+   overlay.classList.add("opacity-70");
+   overlay.classList.remove("pointer-events-none");
+     document.body.style.overflow = "hidden";
 });
 
 closeBtn.addEventListener("click", () => {
   mobileMenu.classList.add("translate-x-full");
   menuBtn.classList.remove("hidden");
+   overlay.classList.add("opacity-0");
+   overlay.classList.remove("opacity-70");
+   overlay.classList.add("pointer-events-none");
+    document.body.style.overflow = "";
 });
 
 menuLinks.forEach(link => {
     link.addEventListener('click', () => {
         mobileMenu.classList.add("translate-x-full");
         menuBtn.classList.remove("hidden");
+        overlay.classList.add("opacity-0");
+        overlay.classList.remove("opacity-70");
+        overlay.classList.add("pointer-events-none");
+        document.body.style.overflow = "";
     })
+})
+
+overlay.addEventListener('click', () => {
+     menuBtn.classList.remove("hidden");
+    mobileMenu.classList.add("translate-x-full");
+     overlay.classList.add("opacity-0");
+     overlay.classList.remove("opacity-70");
+     overlay.classList.add("pointer-events-none");
+     document.body.style.overflow = "";
 })
