0040d62b229310ec4aa904c774631dddeac20340
THESOURCEC0D3 <jonathantamunoiduari@gmail.com>
2025-12-27 00:45:26 +0100

    trying to apply the filtering logic

---
+++ public/js/data.js
@@ -0,0 +1,38 @@
+export const cars = [
+    {
+        id: 1,
+        make: "Toyota",
+        model: "Camry",
+        year: 2020,
+        price: 25000,
+        type: "Sedan",
+        image: "../assets/images/car1.jpg"
+    },
+    {
+        id: 2,
+        make: "Honda",
+        model: "Civic",
+        year: 2019,
+        price: 22000,
+        type: "Sedan",
+        image: "../assets/images/car2.jpg"
+    },
+    {
+        id: 3,
+        make: "Ford",
+        model: "F-150",
+        year: 2021,
+        price: 35000,
+        type: "Truck",
+        image: "../assets/images/car3.jpg"
+    },
+    {
+        id: 4,
+        make: "BMW",
+        model: "X5",
+        year: 2022,
+        price: 65000,
+        type: "SUV",
+        image: "../assets/images/car4.jpg"
+    }
+];
